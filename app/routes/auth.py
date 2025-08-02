b91ad3ba12fdefdd13b0f92868e057ea2af3af73
AlexandrSilich <59784492+AlexandrSilich@users.noreply.github.com>
2025-08-02 23:32:08 +0300

    Update auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -19,7 +19,7 @@ class Token(BaseModel):
     token_type: str = "bearer"
     welcome_message: str
 
-@router.post("/register", response_model=dict)
+@router.post("/register", response_model=dict, summary="Регистрация нового пользователя", description="Регистрация нового пользователя. Логин не должен совпадать с текущими пользователями.")
 def register(user: UserCreate, db: Session = Depends(database.get_db)):
     # Проверяем, существует ли пользователь
     db_user = db.query(models.User).filter(models.User.username == user.username).first()
@@ -32,7 +32,7 @@ def register(user: UserCreate, db: Session = Depends(database.get_db)):
     auth.create_user(db, user.username, user.password)
     return {"message": "Пользователь успешно зарегистрирован"}
 
-@router.post("/login", response_model=Token)
+@router.post("/login", response_model=Token, summary="Операция логина в систему", description="Логин в систему под зарегистрированным ранее пользователем или под дефолтными пользователями указанными в описании. \nПосле успешного логина выдается Bear token.")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     # Аутентифицируем пользователя
     user = auth.authenticate_user(db, form_data.username, form_data.password)
@@ -50,7 +50,7 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
     
     return {"token": token, "token_type": "bearer", "welcome_message": welcome_message}
 
-@router.post("/delete-user", response_model=dict)
+@router.post("/delete-user", response_model=dict, summary="Удаления пользователя", description="Удаления пользователя из системы, используя логин и пароль данного пользователя.")
 def delete_user(user: UserDelete, db: Session = Depends(database.get_db)):
     # Аутентифицируем пользователя
     authenticated_user = auth.authenticate_user(db, user.username, user.password)
